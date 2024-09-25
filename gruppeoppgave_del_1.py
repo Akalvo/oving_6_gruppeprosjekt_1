c37b5145c931974de3f472fbb4bdd20ed21557d2
Akalvo <237947@uis.no>
2024-09-25 15:10:35 +0200

    Update gruppeoppgave_del_1.py

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -1,6 +1,21 @@
 import temperatur_trykk_met_samme_rune_time_datasett
 
-for i in range(10):
-    Sug kuk.
+#for i in range(10):
+    #Sug kuk.
     
+
+#Andreas
+import matplotlib.pyplot as plt
+import datetime
+
+temperaturer = list()
+temperaturer.append
+tidspunkt = list()
+tidspunkt = datetime.datetime()
+
+for linje in temperatur_trykk_met_samme_rune_time_datasett:
+    linje = list()
+    linje.append[0]
+print(linje)
+
 
